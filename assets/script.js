cb8a0093e321cea07a4e7c862e7c8a0fd9333ab6
vancerweston <vweston@student.neumont.edu>
2020-03-06 13:51:32 -0700

    finished style.css, added functions for navigating the forcast bar.

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,24 @@ var date = new Date();
 loadData();
 
 function loadData() {
-    let url = 'http://api.openweathermap.org/data/2.5/forecast?q=Layton,us&appid=59d980accd13058abc9c877e35b141c1';
+    let url = 'http://api.openweathermap.org/data/2.5/forecast?q=Layton,us&units=imperial&appid=59d980accd13058abc9c877e35b141c1';
     request.open('GET', url, true);
     request.onload = loadComplete;
     request.send();
 }
 
+
 function loadComplete(evt) {
     weatherData = JSON.parse(request.responseText);
     console.log(weatherData);
+    let currentWeather = weatherData.list[0];
+    let newList = weatherData.list.filter(item => item.dt_txt.includes('12:00:00'));
+    console.log(newList);
+    newList.unshift(currentWeather);
+    console.log(newList);
+
+
+    
     function updatingData() {
         let layton;
         let weatherDate;
@@ -26,96 +35,52 @@ function loadComplete(evt) {
 
         let info;
 
-        let days = new Array(40);
+        let days = new Array(6);
         let date;
         let weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
         for(i=0; i< days.length; i++){
-            date = new Date(weatherData.list[i].dt_txt);
+            date = new Date(newList[i].dt_txt);
 
             layton = weatherData.city.name;
-            weatherDate = weekday[date.getDay()]+'/'+date.getDay()+'/'+date.getFullYear()+'<br/>'+date.getHours()+':'+date.getMinutes()+'0';
-            listWeatherMain = weatherData.list[i].weather[0].icon;
-            listWeatherDescript = weatherData.list[i].weather[0].description;  
-            listMainTemp = weatherData.list[i].main.temp;
-            windSpeed = weatherData.list[i].wind.speed;
-            pressure = weatherData.list[i].main.pressure;
-            humidity = weatherData.list[i].main.humidity;
-            search = new Date(weatherData.list[i].dt_txt);
+            weatherDate = month[date.getMonth() % 12]+' '+date.getDate() + ' ' +weekday[date.getDay() % 7];
+            weatherDay = weekday[date.getDay() % 7];
+            listWeatherMain = newList[i].weather[0].icon;
+            listWeatherDescript = newList[i].weather[0].description;  
+            listMainTemp = newList[i].main.temp;
+            windSpeed = newList[i].wind.speed;
+            pressure = newList[i].main.pressure;
+            humidity = newList[i].main.humidity;
 
-            info = [layton, weatherDate, listWeatherMain, listWeatherDescript, listMainTemp, windSpeed, pressure, humidity, search];
+            info = [layton, weatherDate, listWeatherMain, listWeatherDescript, listMainTemp, windSpeed, pressure, humidity, weatherDay];
 
             days[i] = info;
-        
-            console.log(days[i][8]);
-            console.log(JSON.stringify(days[i][8]));
-            
-
-
-            //JSON.stringify(days[i][8]) is var search. Which gives us the dt_txt of 
-            //  each object per 3 hours. I need to find a way to get the time out of 
-            //  days[i][8] (12:00:00) and then write an if statement for only 12. 
-            //  I want to then assign that array to an array called week and then push 
-            // week to the html. Struggling on how to grab just the time from days[i][8].
-            // Use a for loop with an if statement, if true assign to week. Then update 
-            // code below.
-
-
         }
-        
-        
-
-        // for(i=0; i<days.length; i++){
-        //     if(){
-        //         console.log(`It worked for ${i}`);
-        //     }else{
-        //         console.log('It didnt work');
-        //     }
-        
-        // }
+        console.log(days);
+        console.log(days[0]);
+    
         
         let data;
         let img;
-        let week = new Array(5);
-
-        for(i=0; i<week.length; i++){
+        for(i=0; i<days.length; i++){
             data = days[i];
             img = data[2];
 
             document.getElementById(`place${i}`).innerHTML = data[0];
-            document.getElementById(`day${i}`).innerHTML = data[1];
-            document.getElementById(`currentTemp${i}`).innerHTML = data[4];
-            document.getElementById(`windSpeed${i}`).innerHTML = data[5];
-            document.getElementById(`pressure${i}`).innerHTML = data[6];
-            document.getElementById(`humidity${i}`).innerHTML = data[7];
-            document.getElementById(`conditions${i}`).innerHTML = `<img src='http://openweathermap.org/img/w/${img}.png'/>`;
+            document.getElementById(`date${i}`).innerHTML = data[1];
+            document.getElementById(`currentTemp${i}`).innerHTML = Math.round(data[4])+'°';
+            document.getElementById(`windSpeed${i}`).innerHTML = `<img class='img' src='/assets/img/wind-speed.png'/>` + ' ' + Math.round(data[5])+' mph';
+            document.getElementById(`pressure${i}`).innerHTML = `<img class='img' src='/assets/img/pressure.png'/>` + ' ' + data[6]+' hpa';
+            document.getElementById(`humidity${i}`).innerHTML = `<img class='img' src='/assets/img/humidity.png'/>` + ' ' + data[7]+' %';
+            document.getElementById(`conditions${i}`).innerHTML = `<img id='weatherIcon' src='http://openweathermap.org/img/w/${img}.png'/>`;
             document.getElementById(`conditionsDesc${i}`).innerHTML = data[3];
 
 
-            //         <div id="place0"></div>
-            //         <div id="day0"></div>
-            //         <div id="currentTemp0"></div>
-            //         <div id='windSpeed0'></div>
-            //         <div id='pressure0'></div>
-            //         <div id='humidity0'></div>
-            //         <div id="conditions0"></div>
-            //         <div id="conditionsDesc0"></div>
-
-            // document.getElementById(`currentTemp${i}`).innerHTML = data[2];
-            // document.getElementById(`conditions${i}`).innerHTML = data[3];
-            // document.getElementById(`conditionsDesc${i}`).innerHTML = data[4];
-            // document.getElementById(`windSpeed${i}`).innerHTML = data[5];
-            // document.getElementById(`pressure${i}`).innerHTML = data[6];
-            // document.getElementById(`humidity${i}`).innerHTML = data[7];
         
-            // document.getElementById(`place1${i}`).innerHTML = data[0];
-            // document.getElementById(`day1${i}`).innerHTML = data[1];
-            // document.getElementById(`currentTemp1${i}`).innerHTML = data[2];
-            // document.getElementById(`conditions1${i}`).innerHTML = data[3];
-            // document.getElementById(`conditionsDesc1${i}`).innerHTML = data[4];
-            // document.getElementById(`windSpeed1${i}`).innerHTML = data[5];
-            // document.getElementById(`pressure1${i}`).innerHTML = data[6];
-            // document.getElementById(`humidity1${i}`).innerHTML = data[7];
+            document.getElementById(`week1${i}`).innerHTML = data[8];
+            document.getElementById(`weekConditions1${i}`).innerHTML = `<img src='http://openweathermap.org/img/w/${img}.png'/>`;
+            document.getElementById(`weekTemp1${i}`).innerHTML = Math.round(data[4])+' °';
 
         }
 
@@ -125,5 +90,101 @@ function loadComplete(evt) {
 
 }
 
+// Functions to navigate the week forecast
 
+function navigationOne() {
+    document.getElementById('dayOne').style.display = 'grid';
+    document.getElementById('dayTwo').style.display = 'none';
+    document.getElementById('dayThree').style.display = 'none';
+    document.getElementById('dayFour').style.display = 'none';
+    document.getElementById('dayFive').style.display = 'none';
+    document.getElementById('daySix').style.display = 'none';
 
+    document.getElementById('forecastOne').style.borderTop = 'solid 5px #4D6466';
+    document.getElementById('forecastTwo').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastThree').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFour').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFive').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastSix').style.borderTop = 'solid 1px #789E9E';
+
+}
+
+function navigationTwo() {
+    document.getElementById('dayOne').style.display = 'none';
+    document.getElementById('dayTwo').style.display = 'grid';
+    document.getElementById('dayThree').style.display = 'none';
+    document.getElementById('dayFour').style.display = 'none';
+    document.getElementById('dayFive').style.display = 'none';
+    document.getElementById('daySix').style.display = 'none';
+
+    document.getElementById('forecastOne').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastTwo').style.borderTop = 'solid 5px #4D6466';
+    document.getElementById('forecastThree').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFour').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFive').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastSix').style.borderTop = 'solid 1px #789E9E';
+}
+
+function navigationThree() {
+    document.getElementById('dayOne').style.display = 'none';
+    document.getElementById('dayTwo').style.display = 'none';
+    document.getElementById('dayThree').style.display = 'grid';
+    document.getElementById('dayFour').style.display = 'none';
+    document.getElementById('dayFive').style.display = 'none';
+    document.getElementById('daySix').style.display = 'none';
+
+    document.getElementById('forecastOne').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastTwo').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastThree').style.borderTop = 'solid 5px #4D6466';
+    document.getElementById('forecastFour').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFive').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastSix').style.borderTop = 'solid 1px #789E9E';
+}
+
+function navigationFour() {
+    document.getElementById('dayOne').style.display = 'none';
+    document.getElementById('dayTwo').style.display = 'none';
+    document.getElementById('dayThree').style.display = 'none';
+    document.getElementById('dayFour').style.display = 'grid';
+    document.getElementById('dayFive').style.display = 'none';
+    document.getElementById('daySix').style.display = 'none';
+
+    document.getElementById('forecastOne').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastTwo').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastThree').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFour').style.borderTop = 'solid 5px #4D6466';
+    document.getElementById('forecastFive').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastSix').style.borderTop = 'solid 1px #789E9E';
+}
+
+function navigationFive() {
+    document.getElementById('dayOne').style.display = 'none';
+    document.getElementById('dayTwo').style.display = 'none';
+    document.getElementById('dayThree').style.display = 'none';
+    document.getElementById('dayFour').style.display = 'none';
+    document.getElementById('dayFive').style.display = 'grid';
+    document.getElementById('daySix').style.display = 'none';
+
+    document.getElementById('forecastOne').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastTwo').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastThree').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFour').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFive').style.borderTop = 'solid 5px #4D6466';
+    document.getElementById('forecastSix').style.borderTop = 'solid 1px #789E9E';
+}
+
+function navigationSix() {
+    document.getElementById('dayOne').style.display = 'none';
+    document.getElementById('dayTwo').style.display = 'none';
+    document.getElementById('dayThree').style.display = 'none';
+    document.getElementById('dayFour').style.display = 'none';
+    document.getElementById('dayFive').style.display = 'none';
+    document.getElementById('daySix').style.display = 'grid';
+
+    document.getElementById('forecastOne').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastTwo').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastThree').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFour').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastFive').style.borderTop = 'solid 1px #789E9E';
+    document.getElementById('forecastSix').style.borderTop = 'solid 5px #4D6466';
+}
